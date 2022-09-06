899a1f73abbac67ff578069504f9d8e0289f8b08
Maxim <you@example.com>
2022-09-06 15:14:33 +0300

    1comm

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("m=");
+int m = int.Parse(Console.ReadLine());
+Console.Write("n=");
+int n = int.Parse(Console.ReadLine());
+
+
+int sumof (int m, int n)
+{
+    int numbers = m;
+    
+    if (m == n)
+     return 0;
+    else
+    m++;
+     numbers = m + sumof(m, n);
+     return numbers;
+}
+
+Console.Write(sumof(m - 1, n));
+
+
---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("m=");
+int m = int.Parse(Console.ReadLine());
+Console.Write("n=");
+int n = int.Parse(Console.ReadLine());
+
+funct(m,n);
+int sumof(int m, int n)
+{
+    if (m == 0)
+     return n + 1;
+    else if (n == 0 && m > 0)
+    return sumof(m - 1, 1);
+    else
+    return (sumof(m - 1, sumof(m, n - 1)));
+}
+
+void funct(int m, int n)
+{
+    Console.Write(sumof(m, n)); 
+
+}
+
